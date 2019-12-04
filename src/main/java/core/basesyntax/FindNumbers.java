e1ffcd973f05f1d2a81ecbae8a340f03544b443a
Vadim Chaika <proxik777@gmail.com>
2019-12-04 22:15:11 +0200

    added a solution for find numbers homework

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -13,6 +13,32 @@ public class FindNumbers {
      * числа 96, 18, 26 и 0.</p>
      */
     public int[] getAllNumbers(String text) {
-        return null;
+        StringBuilder digits = new StringBuilder();
+        int k = 0;
+        int j = 0;
+        int i = 0;
+        int numOfDig = 0;
+        for (i = 0; i < text.length(); i++) {
+            if (Character.isDigit(text.charAt(i))) {
+                while (i + 1 < text.length() && Character.isDigit(text.charAt(i + 1))) {
+                    i++;
+                }
+                numOfDig += 1;
+            }
+        }
+        int[] res = new int[numOfDig];
+        for (i = 0; i < text.length(); i++) {
+            if (Character.isDigit(text.charAt(i)) || text.charAt(i) == '-') {
+                digits.append(text.charAt(i));
+                while (i + 1 < text.length() && Character.isDigit(text.charAt(i + 1))) {
+                    i++;
+                    digits.append(text.charAt(i));
+                }
+                res[j] = Integer.parseInt(digits.toString()) * 2;
+                j++;
+            }
+            digits = new StringBuilder();
+        }
+        return res;
     }
 }
